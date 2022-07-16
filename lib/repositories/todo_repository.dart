b7dfd6f866fa32e5214a4dfff155c09bd0970040
Tiago Ferrari <tiago-projects@outlook.com>
2022-07-16 20:07:08 -0300

    métodos Json para ler a lista de tarefa

--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -1,9 +1,22 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
+import '../models/todo.dart';
+
+const todoListKey = 'todo_list';
 
 class TodoRepository {
-  TodoRepository() {
-    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
+  late SharedPreferences sharedPreferences;
+
+  Future<List<Todo>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final List jsonDecoder = json.decode(jsonString) as List;
+    return jsonDecoder.map((e) => Todo.fromJson(e)).toList();
   }
 
-  late SharedPreferences sharedPreferences;
+  void saveTodoList(List<Todo> todos) {
+    final String jsonString = json.encode(todos);
+    sharedPreferences.setString(todoListKey, jsonString);
+  }
 }
